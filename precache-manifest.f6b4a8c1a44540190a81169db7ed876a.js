70c04c22742d792c4b4ebce6c11bb5b64872d504
Christina Greene <christina.greene@rogers.com>
2019-06-03 12:09:57 -0400

    Updates

---
+++ precache-manifest.f6b4a8c1a44540190a81169db7ed876a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09cf6c1bc55ec8b2f0422c35ba2dc267",
+    "url": "/christinaGreeneProject5/index.html"
+  },
+  {
+    "revision": "828fd7a1bd71ad6fafe6",
+    "url": "/christinaGreeneProject5/static/css/main.4f7b0c63.chunk.css"
+  },
+  {
+    "revision": "d5a9b5673123c2496620",
+    "url": "/christinaGreeneProject5/static/js/2.7a0a1757.chunk.js"
+  },
+  {
+    "revision": "828fd7a1bd71ad6fafe6",
+    "url": "/christinaGreeneProject5/static/js/main.da6bca1b.chunk.js"
+  },
+  {
+    "revision": "9290279e975c2c7d4555",
+    "url": "/christinaGreeneProject5/static/js/runtime~main.bbf9b624.js"
+  },
+  {
+    "revision": "505111cbfd73b2b487fa7b821c3e6522",
+    "url": "/christinaGreeneProject5/static/media/staticBackground.505111cb.jpg"
+  }
+]);
